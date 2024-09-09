3492d26437609397231fa832dc1eb9bc006558ac
KJSDR <killian.delamarre-ruer@students.dominican.edu>
2024-09-09 02:51:36 -0700

    madlib.py assignment

---
+++ madlib.py
@@ -0,0 +1,84 @@
+
+# Homework: Create a madlib. Imagine a story where some of the words are 
+# supplied by user input. Using python you will use input to collect 
+# words for a story and then display the story. 
+
+# Use input to collect each word to a variable
+# Use an f string to display the story
+
+# Your madlib must collect at least 6 words!
+
+
+
+
+
+first_name = input("Enter your first name")
+city_country = input("Enter the city and country you are from")
+favorite_hobby = input("Enter your favorite hobby")
+favorite_meal = input("Enter your favorite meal")
+favorite_holiday = input("Enter your favorite holiday")
+thing_without = input("Enter something you could not live without")
+
+
+
+
+
+print(f"Hello my name is {first_name}, I am from {city_country} and my favorite hobby is {favorite_hobby}. My favorite meal is {favorite_meal} and my favorite holiday is {favorite_holiday}. One thing I could not live without is {thing_without}.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# --------------------------------------------------
+# Partial solution
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# name = input("Name:")
+# color = input("color:")
+# num = input("Number:")
+
+# print(f"{name} wore {color} shoes while they counted to {num}")
